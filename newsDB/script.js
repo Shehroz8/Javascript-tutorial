1ea6c818ba4a8762b58b4eb5c949637dc778d891
Shehroz Raxmatov <98930700+Shehroz8@users.noreply.github.com>
2022-04-27 15:41:20 +0500

    Update script.js

--- newsDB/script.js
+++ newsDB/script.js
@@ -10,8 +10,28 @@ const personalNewDB = {
     privat: false,
 };
 
-const a = prompt("oxirgi ko'rgan yangiliklaringizdan biri"),
+for(let i =0; i<2; i++){
+    const a = prompt("oxirgi ko'rgan yangiliklaringizdan biri"),
     b = prompt("unga qancha baxo bergan bolardingiz");
 
-personalNewDB.news[a] = b;
+    if(a != null && b != null && a != "" && b != "" && a.length < 50){
+        personalNewDB.news[a] = b;
+        console.log("tayor");
+    }else{
+        console.log("error");
+        i--
+    }
+}
+
+if(personalNewDB.count < 10){
+    console.log("juda kam yangilik koripsiz");
+}else if(personalNewDB.count >= 10 && personalNewDB.count <30){
+    console.log("siz klassik tomoshabinsiz");
+}else if(personalNewDB.count >=30){
+    console.log("siz yangiliklar ishqibosisiz");
+}else{
+    alert("xatolik")
+}
+
+
 console.log(personalNewDB);
